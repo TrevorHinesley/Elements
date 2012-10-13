4ffeebc36b6c843ad1daec6a10c72c6b344c1227
Adam Wilczek <arwilczek90@gmail.com>
2012-10-13 15:19:47 -0500

    butts.

--- src/com/hack/elements/view/Main.java
+++ src/com/hack/elements/view/Main.java
@@ -13,51 +13,57 @@ import com.hack.elements.control.ObjectController;
 import com.hack.elements.model.FreeObject;
 import com.hack.elements.model.impl.Direction;
 
-
 public class Main extends BasicGame {
-	
+	int mxpos;
+	int mypos;
 	ObjectController controller;
-	 private TiledMap map;
-    public Main() {
+	private TiledMap map;
+
+	public Main() {
 		super("FUCK!");
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) 
-			throws SlickException
-    {
-         AppGameContainer app = 
-			new AppGameContainer(new Main());
- 
-         app.setDisplayMode(512, 512, false);
-         app.start();
-        
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new Main());
 
-        
+		app.setDisplayMode(512, 512, false);
+		app.start();
 
-
-
-
-         		
-    }
+	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
 		controller = new ObjectController();
-		controller.getModel().getHero().setImage(new Image("resources/images/player-U.png"));
-		 container.setVSync(true);
-         map = new TiledMap("resources/test2.tmx");
+		controller.getModel().getHero()
+				.setImage(new Image("resources/images/player-U.png"));
+		container.setVSync(true);
+		map = new TiledMap("resources/test2.tmx");
 
-
-		 
 	}
-	
+
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
 		FreeObject hero = controller.getModel().getHero();
+		map.render(mxpos, mypos);
 		hero.getImage().draw(hero.getX(), hero.getY());
-		map.render(0, 0);
+
+	}
+
+	public boolean endOfScreen(int xpos, int ypos) {
+		if (xpos % 512 == 0 )
+			
+		if (xpos == map.getWidth())
+			return true;
+		if (ypos % 512 == 0 || ypos == map.getHeight())
+			return true;
+
+		return false;
+	}
+
+	public boolean borderTile(int xpos, int ypos) {
+		return true;
 	}
 
 	@Override
@@ -65,32 +71,29 @@ public class Main extends BasicGame {
 			throws SlickException {
 		Input input = container.getInput();
 		FreeObject hero = controller.getModel().getHero();
-		
-        if(input.isKeyDown(Input.KEY_A))
-        {
-        	FreeObjectController.move(hero, Direction.WEST, .15f);
-        	hero.setImage(new Image("resources/images/player-L.png"));
-        }
-        if(input.isKeyDown(Input.KEY_D))
-        {
-        	FreeObjectController.move(hero, Direction.EAST, .15f);
-        	hero.setImage(new Image("resources/images/player-R.png"));
-        }
- 
-        if(input.isKeyDown(Input.KEY_W))
-        {
-        	FreeObjectController.move(hero, Direction.NORTH, .15f);
-        	hero.setImage(new Image("resources/images/player-U.png"));
-        }
-            
-        if(input.isKeyDown(Input.KEY_S))
-        {
-        	FreeObjectController.move(hero, Direction.SOUTH, .15f);
-        	hero.setImage(new Image("resources/images/player-D.png"));
-        }
-        if(input.isKeyDown(Input.KEY_SPACE))
-        { }
-        if(input.isKeyDown(Input.KEY_ESCAPE))
-        { }
-	}	
+
+		if (input.isKeyDown(Input.KEY_A)) {
+
+			FreeObjectController.move(hero, Direction.WEST, .15f);
+			hero.setImage(new Image("resources/images/player-L.png"));
+		}
+		if (input.isKeyDown(Input.KEY_D)) {
+			FreeObjectController.move(hero, Direction.EAST, .15f);
+			hero.setImage(new Image("resources/images/player-R.png"));
+		}
+
+		if (input.isKeyDown(Input.KEY_W)) {
+			FreeObjectController.move(hero, Direction.NORTH, .15f);
+			hero.setImage(new Image("resources/images/player-U.png"));
+		}
+
+		if (input.isKeyDown(Input.KEY_S)) {
+			FreeObjectController.move(hero, Direction.SOUTH, .15f);
+			hero.setImage(new Image("resources/images/player-D.png"));
+		}
+		if (input.isKeyDown(Input.KEY_SPACE)) {
+		}
+		if (input.isKeyDown(Input.KEY_ESCAPE)) {
+		}
+	}
 }
